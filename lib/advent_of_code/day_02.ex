2504a85a1e2d7ea63ea8f6c67c7d0370c315761f
Logan Kopas <logan.kopas@andgosystems.com>
2025-12-02 16:44:19 -0700

    day2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,77 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def is_duplicated?(str) when is_bitstring(str) do
+    length = String.length(str)
+    if Integer.mod(length, 2) == 1 do
+      false
+    else 
+      half = String.slice(str, 0, div(length, 2))
+      half <> half == str
+    end
+  end
+  def is_duplicated?(int) when is_integer(int) do
+    Integer.to_string(int) |> is_duplicated?()
   end
 
-  def part2(_args) do
+
+  def str_to_range(str) do
+    [ first_ , last_ ] = String.split(str, "-", parts: 2)
+
+    {first, ""} = Integer.parse(first_)
+    {last, _} = Integer.parse(last_)  # allow newline
+
+
+    first..last
+  end
+
+  def get_duplicates_in_range(range) do
+
+    Enum.filter(range, &is_duplicated?/1)
+  end
+
+  def is_multiduplicated?(int) when is_integer(int) do
+    Integer.to_string(int) |> is_multiduplicated?()
+  end
+  def is_multiduplicated?(str) when is_bitstring(str) do
+    length = String.length(str)
+    if length == 1 do
+      false
+    else
+      half_length = div(length, 2)
+      substrings = Enum.map(1..half_length, &(String.slice(str, 0, &1)) )
+      substrings
+        |> Enum.map(&(string_made_of_groups?(&1, str)))
+        |> Enum.any?()
+    end
+  end
+
+  def string_made_of_groups?(group, str) do
+    String.replace(str, group, "") == ""
+  end
+
+  def get_multiduplicates_in_range(range) do
+    Enum.filter(range, &is_multiduplicated?/1) 
+  end
+
+
+  def part1(args) do
+
+    str_ranges = args 
+      |> String.split(",")
+
+    ranges = Enum.map(str_ranges, &str_to_range/1)
+
+    invalid_products = Enum.flat_map(ranges, &get_duplicates_in_range/1)
+    Enum.sum(invalid_products)
+
+  end
+
+  def part2(args) do
+    str_ranges = args 
+      |> String.split(",")
+
+    ranges = Enum.map(str_ranges, &str_to_range/1)
+
+    invalid_products = Enum.flat_map(ranges, &get_multiduplicates_in_range/1)
+    Enum.sum(invalid_products)
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  def input() do
+    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "part1" do
+    result = part1(input())
 
-    assert result
+    assert result == 1227775554
+  end
+
+  test "part2" do
+    result = part2(input())
+
+    assert result == 4174379265
   end
 end
